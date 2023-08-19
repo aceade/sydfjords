3ab3d27dbe3d4adb1a31534d1a70d01183630e04
aceade@github.com <philiprowlands90@gmail.com>
2023-08-19 09:17:41 +0100

    #2 removing image sizes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,34 +5,34 @@ const gallery: ImageDetails[] = [{
   source: "/sydfjords/borealis-480w.webp",
   caption: "The aurora australis over the fjords. Nulla pharetra diam sit amet nisl suscipit adipiscing. Augue lacus viverra vitae congue eu consequat ac felis.",
   altText: "An aurora australis.",
-  srcset: "/sydfjords/borealis-480w.webp 480w, /sydfjords/borealis-800w.webp 800w, /sydfjords/borealis-1366w.webp 1366w, /sydfjords/borealis-1920w.webp 1920w",
-  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1366px, 1920px"
+  srcset: "/sydfjords/borealis-480w.webp 480w, /sydfjords/borealis-800w.webp 800w, /sydfjords/borealis-1080w.webp 1080w",
+  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
 },{
   source: "/sydfjords/town2-480w.webp",
   altText: "A collection of buildings near the water's edge, in front of a mountain",
   caption: "Coldvatn, the heart of the Sydfjords. Sed adipiscing diam donec adipiscing tristique risus nec. Risus feugiat in ante metus dictum at.",
-  srcset: "/sydfjords/town2-480w.webp 480w, /sydfjords/town2-800w.webp 800w, /sydfjords/town2-1080w.webp 1080w, /sydfjords/town2-1366w.webp 1366w, /sydfjords/town2-1920w.webp 1920w",
-  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px, 1366px, 1920px"
+  srcset: "/sydfjords/town2-480w.webp 480w, /sydfjords/town2-800w.webp 800w, /sydfjords/town2-1080w.webp 1080w",
+  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
 },{
   source: "/sydfjords/town-480w.webp",
   altText: "A town near the water's edge, with snow-capped mountains in the background",
   caption: "Coldwatvn, the heart of the Sydfjords. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque purus semper eget duis at",
-  srcset: "/sydfjords/town-480w.webp 480w, /sydfjords/town-800w.webp 800w, /sydfjords/town-1080w.webp 1080w, /sydfjords/town-1366w.webp 1366w, /sydfjords/town-1920w.webp 1920w",
-  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px, 1366px, 1920px"
+  srcset: "/sydfjords/town-480w.webp 480w, /sydfjords/town-800w.webp 800w, /sydfjords/town-1080w.webp 1080w",
+  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
 },{
   source: "/sydfjords/horses-480w.webp",
   altText: "A pair of horses",
   caption: "Tortor at auctor urna nunc id cursus metus. A pellentesque sit amet porttitor eget dolor. Tempor orci eu lobortis elementum nibh tellus.",
-  srcset: "/sydfjords/horses-480w.webp 480w, /sydfjords/horses-800w.webp 800w, /sydfjords/horses-1366w.webp 1366w, /sydfjords/horses-1920w.webp 1920w",
-  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px, 1366px, 1920px"
+  srcset: "/sydfjords/horses-480w.webp 480w, /sydfjords/horses-800w.webp 800w, /sydfjords/horses-1080w.webp 1080w",
+  sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
 }, ];
 
 </script>
 
 <template>
-    <img id="hero" srcset="/hero-480w.webp 480w, /hero-800w.webp 800w, /hero-1366w.webp 1366w, /hero-1920w.webp 1920w"
-     sizes="(max-width: 600px) 480px, 800px, 1366px, 1920px"
-     src="/hero-800w.webp"
+    <img id="hero" srcset="/hero-480w.webp 480w, /hero-800w.webp 800w, /hero-1080w.webp 1080w, /hero-1366w.webp 1366w, /hero-1920w.webp 1920w"
+     sizes="(max-width: 600px) 480px, 800px, 1080px, 1366px, 1920px"
+     src="/hero-480w.webp"
      alt="Snowy mountains above a lake in a wild setting">
      <h1>Welcome to a land steeped in magic</h1>
     <GalleryWithHover v-bind:images="gallery"></GalleryWithHover>
