2f6b2682d710986dded3e7950eb3d22217a8940f
aceade@github.com <philiprowlands90@gmail.com>
2023-09-13 10:50:10 +0100

    #11 fixing alignments

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,19 +6,20 @@
         <h2>Tourism offices</h2>
         <div id="offices">
             <div class="office">
-                <p>Tinland</p>
+                <p class="officeTitle">Tinland</p>
                 <p>Grand Line Station</p>
             </div>
             <div class="office">
-                <p>Levarche</p>
+                <p class="officeTitle">Levarche</p>
                 <p>11 Rucks Bridge</p>
             </div>
             <div class="office">
-                <p>Colwdvatn</p>
+                <p class="officeTitle">Colwdvatn</p>
                 <p>3 Main Square</p>
             </div>
             <div class="office">
-                <p>Kerbalstead</p>
+                <p class="officeTitle">Kerbalstead</p>
+                <p>Einagatan</p>
             </div>
         </div>
 
@@ -26,23 +27,71 @@
         <div id="emailForm">
             <form>
                 <label for="name">Your name</label>
-                <input id="name" required>
+                <input id="name" required minlength="1">
                 <label for="email">Your address</label>
                 <input type="email" id="email" required>
                 <label for="message">Your query</label>
-                <textarea id="message" required></textarea>
+                <textarea id="message" required minlength="1"></textarea>
+                <button @click="mockSend">Submit</button>
+                <p id="submitStatus"></p>
             </form>
+        </div>
+
+        <h2>About (for real)</h2>
+        <div id="about">
+            <p>This is a practice project to get better at responsive web design. It was originally derived from <a href="https://www.frontendpractice.com/projects/monogram">this practice project</a>, but I decided to create a tourism page for a fictional setting that is part Patagonia, part Iceland.</p>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+
+function mockSend(event: Event) {
+    event.preventDefault();
+
+    // one downside to TypeScript: I have to do all this casting crap
+    let name: string = (document.getElementById("name") as HTMLInputElement).value;
+    let email: string = (document.getElementById("email") as HTMLInputElement).value;
+    let message: string = (document.getElementById("message") as HTMLTextAreaElement).value;
+
+    let validation = validateDetails(name, email, message);
+
+    if (validation.nameValid && validation.emailValid && validation.messageValid) {
+        let delay: number = Math.random() * 3000;
+        setTimeout(()=>{
+            notifyResult("Your message has been sent");
+        }, delay);
+    } else {
+        let message = "Please fill out all fields";
+        notifyResult(message);
+    }
+
+}
+
+function validateDetails(name: string, email: string, message: string) {
+    console.log(name, email, message);
+    return {
+        nameValid: name?.length > 1,
+        emailValid: email?.length > 1,
+        messageValid: message?.length > 1
+    }
+}
+
+function notifyResult(result: string) {
+    let submitStatus = (document.getElementById("submitStatus") as HTMLParagraphElement);
+    submitStatus.textContent = result;
+    setTimeout(() => {
+        submitStatus.textContent = "";
+    }, 3000);
+}
+
+
 </script>
 
 <style scoped>
 
     #container {
-        margin: 100px 10%;
+        margin-top: 100px;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -53,29 +102,58 @@
         display: grid;
         grid-template-columns: 1fr 1fr;
         gap: 2rem;
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+        border-bottom: 3px solid #1a2456;
+        border-radius: 0px 0px 10px 10px;
     }
 
     .office {
-        border-radius: 0px 15px 15px 0px;
+        border-radius: 0px 15px 15px 15px;
         background-color: #1a2456;
         color: white;
         padding: 5px;
     }
 
+    .officeTitle {
+        font-style: italic;
+    }
+
     #emailForm {
-        max-width: 320px;
+        max-width: 310px;
         border-radius: 15px;
         background-color: #ff8b68;
         padding: 10px;
         text-align: left;
     }
 
-    #emailForm input, label, textarea {
+    /* Using border-box to keep things consistently sized */
+    #emailForm input, label, textarea, button, p {
         width: 100%;
         margin-bottom: 10px;
+        border-radius: 5px;
+        box-sizing: border-box;
     }
 
     #emailForm label {
         font-size: 1.2rem;
     }
+    #emailForm button, p {
+        font-size: 1.3rem;
+    }
+
+    @media screen and (min-width: 600px) and (max-width: 900px) {
+        #container {
+            margin-left: 100px;
+            margin-right: 100px;
+        }
+    }
+
+    @media screen and (min-width: 901px) {
+        #container {
+            margin-left: 200px;
+            margin-right: 200px;
+        }
+    }
+
 </style>
