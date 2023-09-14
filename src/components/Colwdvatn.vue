68360278746e0003e2190dc2ba3a0f94461b573a
aceade@github.com <philiprowlands90@gmail.com>
2023-09-14 11:45:08 +0100

    #18 specifying full URLs

--- src/components/Colwdvatn.vue
+++ src/components/Colwdvatn.vue
@@ -6,11 +6,11 @@
         <h1>Colwdvatn</h1>
         <p class="blurb">The heart of the Sydfjords</p>
 
-        <img srcset="/town-480w.webp 480w, /town-800w.webp 800w, /town-1080w.webp 1080w, /town-1366w.webp 1366w, /town-1920w.webp 1920w"
+        <img srcset="/sydfjords/town-480w.webp 480w, /sydfjords/town-800w.webp 800w, /sydfjords/town-1080w.webp 1080w, /sydfjords/town-1366w.webp 1366w, /sydfjords/town-1920w.webp 1920w"
     sizes="(max-width: 600px) 480px, (max-width: 900px) 800px, 1366px, 1920px" alt="Town">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc congue nisi vitae suscipit tellus mauris a diam. Sollicitudin tempor id eu nisl nunc mi. Habitant morbi tristique senectus et netus. Eu facilisis sed odio morbi quis commodo odio. Ut consequat semper viverra nam. Lacus vestibulum sed arcu non odio euismod lacinia. Et tortor at risus viverra adipiscing at in tellus. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Suspendisse ultrices gravida dictum fusce ut. Nulla facilisi nullam vehicula ipsum a arcu. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam.</p>
 
-        <img loading="lazy" srcset="/town2-480w.webp 480w, /town2-800w.webp 800w, /town2-1080w.webp 1080w, /town2-1366w.webp 1366w, /town2-1920w.webp 1920w"
+        <img loading="lazy" srcset="/sydfjords/town2-480w.webp 480w, /sydfjords/town2-800w.webp 800w, /sydfjords/town2-1080w.webp 1080w, /sydfjords/town2-1366w.webp 1366w, /sydfjords/town2-1920w.webp 1920w"
     sizes="(max-width: 600px) 480px, (max-width: 900px) 800px, 1366px, 1920px" alt="Town">
     </div>
 </template>
